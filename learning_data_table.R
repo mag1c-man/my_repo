46e3704ce9263b179d2662457a230bdee5350353
mag1c-man <zakhirsch97@gmail.com>
2022-04-11 08:33:37 +0300

    did more stuff

--- learning_data_table.R
+++ learning_data_table.R
@@ -5,7 +5,37 @@
 
 #+ r setup, include = FALSE
 knitr::opts_chunk$set(echo = TRUE)
+library("data.table")
+library("here")
 
 
 #+ r doing stuff
 
+DT <- data.table(Fruit = rep(c("banana", "apple", "orange"), 3:1),
+                 Year  = c(2008, 2009, 2010, 2009, 2010, 2010),
+                 Count = 1:6)
+DT
+
+class(DT)
+
+DT[1:2,2:3]
+
+#' ##select rows
+
+DT[Fruit == "banana",]
+
+#' ##order
+
+DT[order(Fruit),]
+
+DT[order(Fruit, -Year)]
+
+#' ##sample
+
+DT[sample(.N, 3)]
+
+#' ##select columns
+
+DT[, Count]
+
+DT[, .(Count)]
